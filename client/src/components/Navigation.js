db38f867e64f9f85712dcced3539214eeffa24db
Kevin Booth <kdbooth97@gmail.com>
2020-03-10 12:52:26 -0400

    Update styles

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,7 +5,7 @@ class Navigation extends Component {
     render() {
         return(
             <nav>
-                <div>Frugalify</div>
+                <div class="nav__logo">frugalify</div>
             </nav>
         );
     }
