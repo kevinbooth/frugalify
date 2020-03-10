33e6f059b4acc2911f9df5bc0aab88cbe3cb1f82
Kevin Booth <kdbooth97@gmail.com>
2020-03-10 12:47:05 -0400

    Pull in new components into main application

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+// Components
+import Navigation from './components/Navigation';
+import Calendar from './components/Calendar';
+import Category from './components/Category';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navigation />
+      <div class="container">
+        <Calendar />
+        <Category type={'Paycheck'} />
+      </div>
     </div>
   );
 }
